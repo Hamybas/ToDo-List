83731f75ba94a31a9a9676e4e49dc550cce0d553
Mobas <gadiner1@gmail.com>
2023-11-10 15:30:22 +0300

    Implemented the 'Add' button action

--- gui.py
+++ gui.py
@@ -5,19 +5,25 @@ label = sg.Text("Type in a To-Do")
 input_box = sg.InputText(tooltip='Enter To-Do', key="todo")
 add_button = sg.Button('Add')
 edit_button = sg.Button('Edit/Change')
+complete_button = sg.Button('Complete/Delete')
+exit_button = sg.Button('Exit')
+
 todos_field = sg.Listbox(values=functions.get_todos(),
                          key='todos',
                          enable_events=True,
                          size=[45, 10])
 
 window = sg.Window('My To-Do App',
-                   [[label], [input_box, add_button], [todos_field, edit_button]],
+                   [[label],
+                    [input_box, add_button],
+                    [todos_field, edit_button, complete_button],
+                    [exit_button]],
                    font=('Helvetica', 20))
 while True:
     event, values = window.read()
-    print(1, event)
-    print(2, values)
-    print(3, values['todos'])
+#    print(1, event)
+#   print(2, values)
+#    print(3, values['todos'])
 
     match event:
         case 'Add':
@@ -42,6 +48,17 @@ while True:
         case 'todos':
             window['todo'].update(value=values['todos'][0])
 
+        case 'Complete/Delete':
+            todo_to_complete = values['todos'][0]
+            todos = functions.get_todos()
+            todos.remove(todo_to_complete)
+
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+            window['todo'].update(values='')
+
+        case 'Exit':
+            break
         case sg.WIN_CLOSED:
             break
 
