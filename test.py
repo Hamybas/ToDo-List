f5046d8a40f0037492a262f62804875d7588127f
Hamybas <150427140+Hamybas@users.noreply.github.com>
2023-12-24 06:25:27 +0300

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-def greet(message):
-    new_message = message.capitalize()
-    return new_message
-
-
-user_entry = input('What greeting do you want? ')
-greeting = greet(user_entry)
-
-print(greeting)
